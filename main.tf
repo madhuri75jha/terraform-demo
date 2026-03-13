d9bfe3b092b65bc00371577cebf2c335e1675e73
madhuri75jha <100013506+madhuri75jha@users.noreply.github.com>
2026-03-14 02:54:31 +0530

    Update key_name for AWS instance resource

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 resource "aws_instance" "demo" {
 	ami = "ami-0f559c3642608c138"
 	instance_type = "t3.micro"
-	key_name = "terraform-key.pem"
+	key_name = "terraform-key"
 	
 	tags = {
 		Name = "jenkins-terraform-demo"
